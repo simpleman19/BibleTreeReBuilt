8e44b6a940b8844a380a23580bdf9b6e854d76a2
Chance Turner <chance.turner@eagles.oc.edu>
2016-11-06 15:07:16 -0600

    Changed Project structure - Feel free to delete uncesseary items

--- BibleTree/Global.asax.cs
+++ BibleTree/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace BibleTree
@@ -12,7 +13,9 @@ namespace BibleTree
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
