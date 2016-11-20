801d44d528b234899bc11467b623db4dd8d13797
Destarianon <destarianon@gmail.com>
2016-11-20 15:40:38 -0600

    Updated SQL services (#17)
    * Fixed badgetype get queries
    Added dates to badgetype model
    Included adding badges to selftest
    Modified badgetype insert queries to support different parameters
    
    * Fixed Badge Queries to use proper table names
    Replaced dates with datetimes in database creation.
    
    * Created get badge by object query
    
    * Fixed badge update query to use proper field names
    Made several varchar fields longer
    Added user_active flag for false "deletion" to be added later

--- BibleTree/Services/SQL/Badge_Update.sql
+++ BibleTree/Services/SQL/Badge_Update.sql
@@ -1,11 +1,11 @@
 ﻿UPDATE [BibleTree].[dbo].[badge]
 SET 
-	[badge_id] = @id,
-	[badge_name] = @name,
-	[badge_description] = @description,
-	[badge_level] = @level,
-	[badge_activeDate] = @activeDate,
-	[badge_expirationDate] = @expirationDate,
-	[badge_gifURL] = @gifURL,
-	[badge_pngURL] = @pngURL
-WHERE [badge].[badge_id] = @id
+	[badge_id] = @badge_id,
+	[badge_name] = @badge_name,
+	[badge_description] = @badge_description,
+	[badge_level] = @badge_level,
+	[badge_activeDate] = @badge_activeDate,
+	[badge_expirationDate] = @badge_expirationDate,
+	[badge_gifURL] = @badge_gifURL,
+	[badge_pngURL] = @badge_pngURL
+WHERE [badge].[badge_id] = @badge_id
