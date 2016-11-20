04f2f55d339b4c4ed0ced55de2c8b287a1643501
Tyler Kerr <tyler.kerr@eagles.oc.edu>
2016-11-20 16:17:32 -0600

    Added active flag checks for some get queries. Added active flag set on insert queries

--- BibleTree/Services/SQL/Faculty_GetAll.sql
+++ BibleTree/Services/SQL/Faculty_GetAll.sql
@@ -1,3 +1,4 @@
 ﻿SELECT [faculty].*, [user].* 
 FROM [BibleTree].[dbo].[faculty], [BibleTree].[dbo].[user] 
 WHERE [faculty].[user_id] = [user].[user_id]
+	AND [user].[user_active] = 1
--- BibleTree/Services/SQL/User_GetAll.sql
+++ BibleTree/Services/SQL/User_GetAll.sql
@@ -1,2 +1,3 @@
 ﻿SELECT [user].*
 FROM [BibleTree].[dbo].[user]
+WHERE [user].[user_active] = 1
