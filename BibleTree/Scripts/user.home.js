e29a5a151447a44a077affc92bda61e48289c8fb
Chance Turner <chance.turner@eagles.oc.edu>
2016-11-20 16:06:00 -0600

    Updated a lot

---
+++ BibleTree/Scripts/user.home.js
@@ -0,0 +1,59 @@
+﻿var last_selection = -1
+
+function changeTo(number) {
+    last_selection = number
+    var url = '';
+    if (number == 0) {
+        url = '/badges/badgecreate';
+    } else if (number == 1) {
+        url = '/badges/badgeedit';
+    } else if (number == 2) {
+        url = '/badges/badgelist';
+    } else if (number == 3) {
+        url = '/user/studentlist';
+    } else if (number == 4) {
+        url = '/user/facultylist';
+    }
+    for (i = 0; i < 5; i++) {
+        id_to_change = '#button' + i;
+        $(id_to_change).css({ "background-color": "white" });
+    }
+    id_to_change = '#button' + number;
+    $.ajax({
+        type: "GET",
+        headers: {
+        },
+        url: url,
+        dataType: 'html',
+        success: function (data) {
+            $('#partial_view').html(data);
+        }
+    });
+    $(id_to_change).css({"background-color": "grey"});
+}
+
+function editBadgeGetId() {
+    last_selection = 1;
+    selectBadge();
+}
+
+function editBadge(id) {
+    var url = '/badges/badgeedit/' + id;
+    for (i = 0; i < 5; i++) {
+        id_to_change = '#button' + i;
+        $(id_to_change).css({ "background-color": "white" });
+    }
+    id_to_change = '#button' + 1;
+    $.ajax({
+        type: "GET",
+        headers: {
+        },
+        url: url,
+        dataType: 'html',
+        success: function (data) {
+            $('#partial_view').html(data);
+        }
+    });
+    $(id_to_change).css({ "background-color": "grey" });
+    last_selection = -1;
+}
--- BibleTree/Services/SQL/Badge_GetByObject.sql
+++
@@ -1,9 +0,0 @@
-﻿SELECT [badge].*
-FROM [BibleTree].[dbo].[badge]
-WHERE [badge].[badge_name] = @badge_name
-	AND [badge].[badge_gifURL] = @badge_gifURL
-	AND [badge].[badge_pngURL] = @badge_pngURL
-	AND [badge].[badge_description] = @badge_description
-	AND [badge].[badge_activeDate] = @badge_activeDate
-	AND [badge].[badge_expirationDate] = @badge_expirationDate
-	AND [badge].[badge_level] = @badge_level
