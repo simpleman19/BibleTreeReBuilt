87657c3287d331540fc8020c99db3924ac1b9d2f
Chance Turner <chance.turner@eagles.oc.edu>
2016-12-04 16:02:43 -0600

    Working on csv button

--- BibleTree/Services/SQL/AwardedBadge_GetByUserId.sql
+++ BibleTree/Services/SQL/AwardedBadge_GetByUserId.sql
@@ -1,3 +1,3 @@
 ﻿SELECT [awardedbadge].*, [badge].* 
 FROM [BibleTree].[dbo].[awardedbadge], [BibleTree].[dbo].[badge] 
-WHERE [awardedbadge].[user_id] = @user_id
+WHERE [awardedbadge].[user_id] = @user_id AND [awardedbadge].[badge_id] = [badge].[badge_id]
